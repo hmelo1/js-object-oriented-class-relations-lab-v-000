1f8205516d794c4b721bc23fe9ae9f4df47f8666
Eddie Melo <heriberto@melo.nyc>
2018-05-05 14:18:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,5 +39,10 @@ class Trip{
     this.passengerId = passenger.id
 
     store.trips.push(this);
-  }
-}
+  };
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId;
+    });
+  };
+};
