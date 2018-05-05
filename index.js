816d7fca45448f37a0a6344dbdcf4ed4417d8f8b
Eddie Melo <heriberto@melo.nyc>
2018-05-05 14:05:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let store = {drivers: [], passengers: [], trips: []}
+
 let driverID = 0;
 
 class Driver {
   constructor(name){
     this.id = ++driverID;
     this.name = name;
+
+    store.drivers.push(this);
   };
 };
